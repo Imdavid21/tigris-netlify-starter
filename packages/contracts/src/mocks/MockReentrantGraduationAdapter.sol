b6e30704bc3e5e71c0db6c096a08fc8ed374ad17
Uddalak <84083101+Imdavid21@users.noreply.github.com>
2026-09-14 15:33:25 +0530

    Add reentrant graduation adapter mock

---
+++ packages/contracts/src/mocks/MockReentrantGraduationAdapter.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IGraduationAdapter} from "../interfaces/IGraduationAdapter.sol";
+
+interface IReentryFactory {
+    function createGraduatedPool(address token)
+        external
+        returns (address pool, uint256 positionId);
+}
+
+contract MockReentrantGraduationAdapter is IGraduationAdapter {
+    using SafeERC20 for IERC20;
+
+    IReentryFactory public immutable factory;
+    address public targetToken;
+    bool public reentryBlocked;
+
+    constructor(IReentryFactory factory_) {
+        factory = factory_;
+    }
+
+    function setTargetToken(address token) external {
+        targetToken = token;
+    }
+
+    function createPoolAndLock(
+        address token,
+        address quoteAsset,
+        uint256 tokenAmount,
+        uint256 quoteAmount,
+        address
+    ) external returns (address pool, uint256 positionId) {
+        try factory.createGraduatedPool(targetToken) {
+            reentryBlocked = false;
+        } catch {
+            reentryBlocked = true;
+        }
+
+        IERC20(token).safeTransferFrom(msg.sender, address(this), tokenAmount);
+        IERC20(quoteAsset).safeTransferFrom(msg.sender, address(this), quoteAmount);
+
+        return (address(0x5678), 1);
+    }
+}
