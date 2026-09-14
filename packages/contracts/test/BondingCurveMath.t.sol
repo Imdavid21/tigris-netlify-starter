33936cb984cb64463ea4d050285d27516ad277bb
Uddalak <84083101+Imdavid21@users.noreply.github.com>
2026-09-14 15:03:36 +0530

    Fix fuzz test mutability

--- packages/contracts/test/BondingCurveMath.t.sol
+++ packages/contracts/test/BondingCurveMath.t.sol
@@ -6,16 +6,19 @@ import {BondingCurveMath} from "../src/libraries/BondingCurveMath.sol";
 
 contract BondingCurveMathTest is Test {
     function testAmountOutIncreasesWithInput() public pure {
-        uint256 a = BondingCurveMath.amountOut(100e6, 2_500e6, 1_000_000_000 ether, 100);
-        uint256 b = BondingCurveMath.amountOut(200e6, 2_500e6, 1_000_000_000 ether, 100);
+        uint256 a =
+            BondingCurveMath.amountOut(100e6, 2_500e6, 1_000_000_000 ether, 100);
+        uint256 b =
+            BondingCurveMath.amountOut(200e6, 2_500e6, 1_000_000_000 ether, 100);
         assertGt(b, a);
     }
 
-    function testFuzzOutputNeverExceedsReserve(uint128 amountIn) public pure {
+    function testFuzzOutputNeverExceedsReserve(uint128 amountIn) public {
         vm.assume(amountIn > 0);
         uint256 reserveIn = 2_500e6;
         uint256 reserveOut = 1_000_000_000 ether;
-        uint256 out = BondingCurveMath.amountOut(amountIn, reserveIn, reserveOut, 100);
+        uint256 out =
+            BondingCurveMath.amountOut(amountIn, reserveIn, reserveOut, 100);
         assertLt(out, reserveOut);
     }
 }
