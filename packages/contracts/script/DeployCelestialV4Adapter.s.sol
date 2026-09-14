c0fc41b453331425d77bb4204fdb7076352181dd
Uddalak <84083101+Imdavid21@users.noreply.github.com>
2026-09-14 23:56:59 +0530

    Deploy guarded Celestial v4 pools

--- packages/contracts/script/DeployCelestialV4Adapter.s.sol
+++ packages/contracts/script/DeployCelestialV4Adapter.s.sol
@@ -14,6 +14,10 @@ import {
     ICelestialPermit2
 } from "../src/interfaces/UniswapV4Minimal.sol";
 import {ICelestialGraduationAdapter} from "../src/interfaces/ICelestialGraduationAdapter.sol";
+import {
+    CelestialPoolGuardHook,
+    CelestialPoolGuardHookDeployer
+} from "../src/CelestialPoolGuardHook.sol";
 
 contract DeployCelestialV4Adapter is Script {
     error MissingCode(address target);
@@ -41,6 +45,20 @@ contract DeployCelestialV4Adapter is Script {
         if (factory.owner() != deployer) revert WrongFactoryOwner(deployer, factory.owner());
 
         vm.startBroadcast(deployerKey);
+        CelestialPoolGuardHookDeployer hookDeployer = new CelestialPoolGuardHookDeployer();
+        vm.stopBroadcast();
+
+        bytes32 initCodeHash = keccak256(
+            abi.encodePacked(
+                type(CelestialPoolGuardHook).creationCode,
+                abi.encode(poolManager, deployer)
+            )
+        );
+        bytes32 salt = _findHookSalt(address(hookDeployer), initCodeHash);
+        address predictedHook = _computeCreate2(address(hookDeployer), salt, initCodeHash);
+
+        vm.startBroadcast(deployerKey);
+        CelestialPoolGuardHook hook = hookDeployer.deploy(salt, poolManager, deployer);
 
         connector = new CelestialUniswapV4Connector(
             ICelestialV4PoolManager(poolManager),
@@ -48,14 +66,19 @@ contract DeployCelestialV4Adapter is Script {
             ICelestialV4Quoter(quoter),
             ICelestialUniversalRouter(universalRouter),
             ICelestialPermit2(permit2),
+            address(hook),
             lpFee,
             1
         );
         adapter = new CelestialDexAdapter(address(factory), connector);
+        hook.sealInitializer(address(connector));
         factory.setGraduationAdapter(ICelestialGraduationAdapter(address(adapter)));
+
+        require(address(hook) == predictedHook, "hook address mismatch");
 
         vm.stopBroadcast();
 
+        console2.log("CELESTIAL_V4_GUARD_HOOK", address(hook));
         console2.log("CELESTIAL_V4_CONNECTOR", address(connector));
         console2.log("CELESTIAL_DEX_ADAPTER", address(adapter));
         console2.log("UNISWAP_V4_POOL_MANAGER", poolManager);
@@ -63,6 +86,23 @@ contract DeployCelestialV4Adapter is Script {
         console2.log("UNISWAP_V4_QUOTER", quoter);
         console2.log("UNISWAP_UNIVERSAL_ROUTER", universalRouter);
         console2.log("UNISWAP_PERMIT2", permit2);
+    }
+
+    function _findHookSalt(address deployer, bytes32 initCodeHash) internal pure returns (bytes32 salt) {
+        for (uint256 i; i < type(uint32).max; ++i) {
+            salt = bytes32(i);
+            address predicted = _computeCreate2(deployer, salt, initCodeHash);
+            if ((uint160(predicted) & ((1 << 14) - 1)) == (1 << 13)) return salt;
+        }
+        revert("hook salt not found");
+    }
+
+    function _computeCreate2(address deployer, bytes32 salt, bytes32 initCodeHash)
+        internal
+        pure
+        returns (address)
+    {
+        return address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xff), deployer, salt, initCodeHash)))));
     }
 
     function _requireCode(address target) internal view {
