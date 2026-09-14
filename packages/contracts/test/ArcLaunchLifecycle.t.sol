d2717e82fdfbaa459f8a0512a3eb31bbe10a24e4
Uddalak <84083101+Imdavid21@users.noreply.github.com>
2026-09-14 15:05:57 +0530

    Wire fee escrow and accounting: packages/contracts/test/ArcLaunchLifecycle.t.sol

--- packages/contracts/test/ArcLaunchLifecycle.t.sol
+++ packages/contracts/test/ArcLaunchLifecycle.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.26;
 import {Test} from "forge-std/Test.sol";
 import {ArcLaunchFactory} from "../src/ArcLaunchFactory.sol";
 import {ArcBondingCurve} from "../src/ArcBondingCurve.sol";
+import {ArcFeeEscrow} from "../src/ArcFeeEscrow.sol";
 import {ArcToken} from "../src/ArcToken.sol";
 import {MockUSDC} from "../src/mocks/MockUSDC.sol";
 
@@ -13,11 +14,13 @@ contract ArcLaunchLifecycleTest is Test {
 
     address creator = address(0xCAFE);
     address trader = address(0xBEEF);
+    address treasury = address(0xFEE);
 
     function setUp() public {
         usdc = new MockUSDC();
         factory = new ArcLaunchFactory(
             usdc,
+            treasury,
             2_500e6,
             10_000e6,
             100,
@@ -28,7 +31,8 @@ contract ArcLaunchLifecycleTest is Test {
 
     function testCreateBuySellLifecycle() public {
         vm.prank(creator);
-        (address tokenAddr, address curveAddr) = factory.createToken("Arc Test", "ARCX");
+        (address tokenAddr, address curveAddr) =
+            factory.createToken("Arc Test", "ARCX");
 
         ArcToken token = ArcToken(tokenAddr);
         ArcBondingCurve curve = ArcBondingCurve(curveAddr);
@@ -45,11 +49,50 @@ contract ArcLaunchLifecycleTest is Test {
 
         uint256 sellAmount = token.balanceOf(trader) / 4;
         token.approve(curveAddr, sellAmount);
+
         uint256 sellQuote = curve.quoteSell(sellAmount);
         curve.sell(sellAmount, sellQuote * 99 / 100);
 
         assertGt(usdc.balanceOf(trader), 0);
         vm.stopPrank();
+    }
+
+    function testFeesSweepAndClaim() public {
+        vm.prank(creator);
+        (, address curveAddr) = factory.createToken("Arc Test", "ARCX");
+
+        ArcBondingCurve curve = ArcBondingCurve(curveAddr);
+        ArcFeeEscrow escrow = factory.feeEscrow();
+
+        vm.startPrank(trader);
+        usdc.approve(curveAddr, type(uint256).max);
+        curve.buy(1_000e6, 1);
+        vm.stopPrank();
+
+        curve.sweepFees();
+
+        assertGt(escrow.claimable(creator), 0);
+        assertGt(escrow.claimable(treasury), 0);
+
+        uint256 creatorBefore = usdc.balanceOf(creator);
+        vm.prank(creator);
+        escrow.claim();
+        assertGt(usdc.balanceOf(creator), creatorBefore);
+    }
+
+    function testBuyCapsRealReserveAtGraduationThreshold() public {
+        vm.prank(creator);
+        (, address curveAddr) = factory.createToken("Arc Test", "ARCX");
+
+        ArcBondingCurve curve = ArcBondingCurve(curveAddr);
+
+        vm.startPrank(trader);
+        usdc.approve(curveAddr, type(uint256).max);
+        curve.buy(100_000e6, 1);
+        vm.stopPrank();
+
+        assertEq(curve.trackedQuote(), 10_000e6);
+        assertTrue(curve.readyToGraduate());
     }
 
     function testCannotTradeWithZeroInput() public {
@@ -64,17 +107,20 @@ contract ArcLaunchLifecycleTest is Test {
         vm.assume(amount > 1e6 && amount < 100_000e6);
 
         vm.prank(creator);
-        (address tokenAddr, address curveAddr) = factory.createToken("Arc Test", "ARCX");
+        (address tokenAddr, address curveAddr) =
+            factory.createToken("Arc Test", "ARCX");
 
         ArcBondingCurve curve = ArcBondingCurve(curveAddr);
         ArcToken token = ArcToken(tokenAddr);
 
         usdc.mint(trader, amount);
+
         vm.startPrank(trader);
         usdc.approve(curveAddr, amount);
         try curve.buy(amount, 1) {} catch {}
         vm.stopPrank();
 
         assertGe(token.balanceOf(curveAddr), factory.RESERVED_TOKENS());
+        assertLe(curve.trackedQuote(), factory.graduationThreshold());
     }
 }
