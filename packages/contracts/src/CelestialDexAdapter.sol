4d7c3ee8be7bb97623b57630e416f43744f7f0ba
Uddalak <84083101+Imdavid21@users.noreply.github.com>
2026-09-14 23:44:32 +0530

    Use Celestial price-aware graduation adapter

--- packages/contracts/src/CelestialDexAdapter.sol
+++ packages/contracts/src/CelestialDexAdapter.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.26;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {IGraduationAdapter} from "./interfaces/IGraduationAdapter.sol";
+import {ICelestialGraduationAdapter} from "./interfaces/ICelestialGraduationAdapter.sol";
 import {ICelestialDexConnector} from "./interfaces/ICelestialDexConnector.sol";
 
-contract CelestialDexAdapter is IGraduationAdapter, ReentrancyGuard {
+contract CelestialDexAdapter is ICelestialGraduationAdapter, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     address public immutable factory;
