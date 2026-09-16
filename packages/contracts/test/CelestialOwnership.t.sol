c500857636ec6000673239dc68f4ff77a124120d
Uddalak <84083101+Imdavid21@users.noreply.github.com>
2026-09-16 15:48:13 +0530

    test: cover keeper invalidation after ownership transfer

--- packages/contracts/test/CelestialOwnership.t.sol
+++ packages/contracts/test/CelestialOwnership.t.sol
@@ -8,23 +8,34 @@ import {CelestialBuybackVault} from "../src/CelestialBuybackVault.sol";
 contract CelestialOwnershipTest is Test {
     address internal constant NEXT_OWNER = address(0xBEEF);
     address internal constant KEEPER = address(0xCAFE);
+    address internal constant NEXT_KEEPER = address(0xD00D);
 
-    function testBuybackControlFollowsFactoryOwnership() public {
+    function testBuybackControlFollowsFactoryOwnershipAndInvalidatesOldKeeper() public {
         CelestialLaunchFactory factory = new CelestialLaunchFactory(address(this));
         CelestialBuybackVault vault = factory.buybackVault();
 
         assertEq(vault.owner(), address(factory));
         assertEq(vault.controller(), address(this));
         assertEq(vault.keeper(), address(0));
+        assertEq(vault.keeperController(), address(0));
+
+        vault.setKeeper(KEEPER);
+        assertEq(vault.keeper(), KEEPER);
+        assertEq(vault.keeperController(), address(this));
 
         factory.transferOwnership(NEXT_OWNER);
         assertEq(vault.controller(), NEXT_OWNER);
 
+        vm.prank(KEEPER);
         vm.expectRevert(CelestialBuybackVault.NotAuthorized.selector);
-        vault.setKeeper(KEEPER);
+        vault.executeCurveBuyback(address(0), 0, 0);
+
+        vm.expectRevert(CelestialBuybackVault.NotAuthorized.selector);
+        vault.setKeeper(NEXT_KEEPER);
 
         vm.prank(NEXT_OWNER);
-        vault.setKeeper(KEEPER);
-        assertEq(vault.keeper(), KEEPER);
+        vault.setKeeper(NEXT_KEEPER);
+        assertEq(vault.keeper(), NEXT_KEEPER);
+        assertEq(vault.keeperController(), NEXT_OWNER);
     }
 }
