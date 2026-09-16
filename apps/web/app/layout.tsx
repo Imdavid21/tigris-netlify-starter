7b17bce27e930863deff89f198129e2bf29bd789
Uddalak <84083101+Imdavid21@users.noreply.github.com>
2026-09-16 14:09:28 +0530

    foundation: load Roboto Flex for Material 3 type scale

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -32,6 +32,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,300..800&display=swap"
+          rel="stylesheet"
+        />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
       <body><Providers>{children}</Providers></body>
